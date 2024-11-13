bed6da6378f9e769fe7572e513cbf3c053637039
fabianeoliveirasantos <Fabiane.o.santos2911@gmail.com>
2024-11-13 12:28:19 -0300

    função e testes para exibição de status do aluno

--- src/exibirStatus.js
+++ src/exibirStatus.js
@@ -1,15 +1,16 @@
 const obterStatus = (media) => {
     if (media < 4.0) {
-        return 'Reprovaddo por média'
+      return "Reprovado por média";
     }
-
+  
     if (media >= 6.0) {
-        return 'Aprovado por média'
+      return "Aprovado por média";
     }
-
-    if (media > 4.0 && media < 6.0) {
-        return 'Aguardando a final'
+  
+    if (media >= 4.0 && media < 6.0) {
+      return "Aguardando a final";
     }
-}
-
-module.exports = { obterStatus }
+  };
+  
+  module.exports = { obterStatus };
+  
--- src/lancamentoNotas.js
+++ src/lancamentoNotas.js
@@ -1,25 +1,26 @@
-const lancarNotas = (n1, n2) => {
-  if (typeof n1 !== "number" || typeof n2 !== "number") {
-    throw new Error('Erro: "Entradas devem ser numéricas"');
-  }
+const lancarNotas = ( n1, n2 ) => {
 
-  if (n1 < 0 || n1 > 10) {
-    throw new Error('Erro: "Nota de 1UP inválida"');
-  }
+    if (n1 < 0 || n1 > 10) {
+        throw new Error('Erro: "Nota de 1UP inválida"')
+    }
 
-  if (!(n1 % 1 !== 0 && Math.floor(n1 * 10) / 10 === n1)) {
-    throw new Error('Erro: "Nota de 1UP inválida"');
-  }
+    if (!(n1 % 1 !== 0 && Math.floor(n1 * 10) / 10 === n1)) {
+        throw new Error('Erro: "Nota de 1UP inválida"')
+    }
 
-  if (n2 < 0 || n2 > 10) {
-    throw new Error('Erro: "Nota de 2UP inválida"');
-  }
+    if (n2 < 0 || n2 > 10) {
+        throw new Error('Erro: "Nota de 2UP inválida"')
+    }
 
-  if (!(n2 % 1 !== 0 && Math.floor(n2 * 10) / 10 === n2)) {
-    throw new Error('Erro: "Nota de 2UP inválida"');
-  }
+    if (!(n2 % 1 !== 0 && Math.floor(n2 * 10) / 10 === n2)) {
+        throw new Error('Erro: "Nota de 2UP inválida"')
+    }
 
-  return "Notas salvas com sucesso";
-};
+    if (typeof(n1) !== 'number' || typeof(n2) !== 'number') {
+        throw new Error('Erro: "Entradas devem ser numéricas"')
+    }
 
-module.exports = { lancarNotas };
+    return "Notas salvas com sucesso"
+}
+
+module.exports= { lancarNotas }
