bacdce37e0b275d448b1f54b46c3a1dc8b6d1a6f
fabianeoliveirasantos <Fabiane.o.santos2911@gmail.com>
2024-11-13 12:33:35 -0300

    Reordena validação de valores lancarNotas

--- src/lancamentoNotas.js
+++ src/lancamentoNotas.js
@@ -1,5 +1,9 @@
 const lancarNotas = ( n1, n2 ) => {
 
+    if (typeof(n1) !== 'number' || typeof(n2) !== 'number') {
+        throw new Error('Erro: "Entradas devem ser numéricas"')
+    }
+
     if (n1 < 0 || n1 > 10) {
         throw new Error('Erro: "Nota de 1UP inválida"')
     }
@@ -14,10 +18,6 @@ const lancarNotas = ( n1, n2 ) => {
 
     if (!(n2 % 1 !== 0 && Math.floor(n2 * 10) / 10 === n2)) {
         throw new Error('Erro: "Nota de 2UP inválida"')
-    }
-
-    if (typeof(n1) !== 'number' || typeof(n2) !== 'number') {
-        throw new Error('Erro: "Entradas devem ser numéricas"')
     }
 
     return "Notas salvas com sucesso"
